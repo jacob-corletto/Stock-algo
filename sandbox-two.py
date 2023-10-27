9b5fe091668a0afdcf57560dc0abb1d4f3d7ae26
raxelortiz7 <raxelortiz7@csu.fullerton.edu>
2023-10-26 17:05:14 -0700

    in sandbox two we get better results for one testcase but its not the best, old versions of this code would return this `This size of input array 9 Stocks_and_Values [[4, 5], [5, 6], [1, 10], [2, 3], [7, 10], [8, 9], [2, 4], [3, 12], [7, 14]] as empty but now it returns it as [[4, 5], [5, 6], [1, 10], [8, 9], [7, 14], [7, 14], [7, 14], [7, 14], [7, 14]] which adds up to 100 It fails this case: This size of input array 3 Stocks_and_Values [[3, 3], [2, 3], [5, 3]] this is the amount 8 Sum of the values: 0 [] where it doesnt recoognize that it doesnt have to reach 8 -> best solution can be 6

---
+++ sandbox-two.py
@@ -0,0 +1,66 @@
+class Knapsack:
+    def __init__(self, sublists, capacity):
+        self.sublists = sublists
+        self.capacity = capacity
+        self.best_subset = None
+
+    def generate_knapsack_candidates(self, sublists):
+        candidates = []
+
+        def generate_knapsack(remaining_sublists, remaining_capacity, current_subset, max_item_count=1):
+            if remaining_capacity == 0:
+                candidates.append(current_subset)
+                return
+            for i in range(len(remaining_sublists)):
+                sublist = remaining_sublists[i]
+                if sublist[1] <= remaining_capacity:
+                    new_subset = current_subset + [sublist]
+                    if max_item_count > 1:
+                        generate_knapsack(remaining_sublists[:i] + remaining_sublists[i + 1:], remaining_capacity - sublist[1], new_subset, max_item_count - 1)
+                    else:
+                        generate_knapsack(remaining_sublists[1:], remaining_capacity - sublist[1], new_subset)
+
+        generate_knapsack(sublists, self.capacity, [])
+        return candidates
+
+    def candidate_evaluation(self, candidate_subset):
+        value = sum([sublist[1] for sublist in candidate_subset])
+        return value
+
+    def find_best(self):
+        best_subset = []
+        best_value = 0
+        candidates = self.generate_knapsack_candidates(self.sublists)
+        for candidate in candidates:
+            value = self.candidate_evaluation(candidate)
+            if value > best_value:
+                best_subset = candidate
+                best_value = value
+
+        self.best_subset = best_subset
+        return best_subset
+    
+    
+with open('input.txt', "r") as f:
+    test_case_count = 0
+    for i in range(len('input.txt')):
+        if f.readline() == '\n':
+            var1 = int(f.readline())
+            sublists = eval(f.readline())
+            capacity = int(f.readline())
+
+            print(f'This size of input array {var1}')
+            print(f'Stocks_and_Values {sublists}')
+            print(f'this is the amount {capacity}')
+
+            knapsack = Knapsack(sublists, capacity)
+            best_subset = knapsack.find_best()
+
+            # find the sum of the first index of everysublist in best_subset
+            if best_subset == None:
+                total_value = 0
+            else: total_value = sum([sublist[1] for sublist in best_subset])
+
+            print(f'Sum of the values: {total_value}')
+            print(str(best_subset) + '\n\n')
+
