ba660cd7a034b9c69ff0578e7165d852314b2bb6
marsxnow <raxelportiz@gmail.com>
2023-10-31 09:03:41 -0700

    Knapsack.py Created the function called exhuastive method, it takes in the total number of lists does knapsack on it and it prints the max sum and best combination. I still need to write to an output file.

--- knapsack.py
+++ knapsack.py
@@ -3,27 +3,42 @@ import itertools
 total_sublists = 4
 list = [[1,2],[4,3],[3,6],[6,7], [4,7]]
 capacity = 10
-max_sum = 0
-max_combination = []
 
 # Generate all possible combinations of sublists
-for r in range(1, total_sublists + 1):
-    combinations = itertools.combinations(list, r)
-    
-    # Iterate through each combination
-    for combination in combinations:
-        current_sum = sum(sublist[1] for sublist in combination)
-        current_capacity = sum(sublist[1] for sublist in combination)
+def exhuastive_method(total_sublists, list, capacity):
+    max_sum = 0
+    max_combination = []
+    for r in range(1, total_sublists + 1):
+        combinations = itertools.combinations(list, r)
         
-        # Check if current_sum exceeds max_sum and satisfies capacity constraint
-        if current_sum > max_sum and current_capacity <= capacity:
-            max_sum = current_sum
-            max_combination = combination
+        # Iterate through each combination
+        for combination in combinations:
+            current_sum = sum(sublist[1] for sublist in combination)
+            current_capacity = sum(sublist[1] for sublist in combination)
+            
+            # Check if current_sum exceeds max_sum and satisfies capacity constraint
+            if current_sum > max_sum and current_capacity <= capacity:
+                max_sum = current_sum
+                max_combination = combination
 
-    # Check if the sum of the first values exceeds the capacity
-    if sum(sublist[1] for sublist in max_combination) > capacity:
-        max_sum = 0
-        max_combination = []
+        # Check if the sum of the first values exceeds the capacity
+        if sum(sublist[1] for sublist in max_combination) > capacity:
+            max_sum = 0
+            max_combination = []
 
-print("Maximum sum:", max_sum)
-print("Combination of sublists:", max_combination)
+    print("Maximum sum:", max_sum)
+    print("Combination of sublists:", str(max_combination) + '\n')
+
+with open('input.txt', "r") as f:
+    test_case_count = 0
+    for i in range(len('input.txt')):
+        if f.readline() == '\n':
+            total_sublists = int(f.readline())
+            list = eval(f.readline())
+            capacity = int(f.readline())
+
+            exhuastive_method(total_sublists, list, capacity);
+
+    
+
+
