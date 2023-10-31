8ed45c8df6dcda6d8c81ea0afcc700955db39e5c
marsxnow <raxelportiz@gmail.com>
2023-10-31 09:32:18 -0700

    knapsack.py First half of the project is complete. we write to an output fill called exhaustive_output.txt and the code works for all 10 test cases

--- knapsack.py
+++ knapsack.py
@@ -26,19 +26,35 @@ def exhuastive_method(total_sublists, list, capacity):
             max_sum = 0
             max_combination = []
 
-    print("Maximum sum:", max_sum)
-    print("Combination of sublists:", str(max_combination) + '\n')
-
-with open('input.txt', "r") as f:
-    test_case_count = 0
-    for i in range(len('input.txt')):
-        if f.readline() == '\n':
-            total_sublists = int(f.readline())
-            list = eval(f.readline())
-            capacity = int(f.readline())
-
-            exhuastive_method(total_sublists, list, capacity);
-
+    return max_sum, max_combination
     
+with open('input.txt', "r") as f:
+    test_case_count = 1
+    lines = f.readlines()  # Read all lines into a list
+    i = 0
+
+    while i < len(lines):
+        if lines[i] == '\n':
+            total_sublists = int(lines[i + 1])
+            my_list = eval(lines[i + 2])
+            capacity = int(lines[i + 3])
+
+            with open("exhaustive_output.txt", "a") as output_file:
+                max_sum, max_combination = exhuastive_method(total_sublists, my_list, capacity)
+                output_file.write("Case #" + str(test_case_count) + '\n')
+                output_file.write(str(max_sum) + '\n')
+                output_file.write(str(max_combination) + '\n\n')
+
+            total_sublists = 0
+            my_list = []
+            capacity = 0
+
+            # Update the index to skip the processed lines
+            i += 4
+            test_case_count += 1
+        else:
+            i += 1  # Move to the next line
+
+
 
 
