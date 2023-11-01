9a3ecf9b0f548cd75dd5325a9e20a370b9fbb21f
marsxnow <raxelportiz@gmail.com>
2023-11-01 16:56:41 -0700

    Dynamic Programing  Works for one test case.

--- dynamicprogramming.py
+++ dynamicprogramming.py
@@ -1,8 +1,16 @@
-#We gotta do knwapsack problem but coded dynamically
+#knapsack in dynamic programming
+def knapSack(items, w, n):
+    if n == 0 or w == 0:
+        return 0
+    if (items[n-1][1] > w):
+        return knapSack(items, w, n-1)
+# Driver program to test above function
+    else:
+        return max(items[n-1][1] + knapSack(items, w-items[n-1][1], n-1), knapSack(items, w, n-1))
 
-def knapsack_best_subset_dynamic(sublists, capacity):
-    return 0
-
-
+n = 4
+items = [[1,2],[3,6],[4,3],[6,7]]
+w = 12
+print(knapSack(items, w, n))
 
 
