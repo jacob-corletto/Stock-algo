0ccefaa4e6bbd4c8756ba275b1b8f70113f3bcef
marsxnow <raxelportiz@gmail.com>
2023-11-02 17:09:33 -0700

    Add dynamic programming solution for knapsack problem with input and output file handling

--- dynamicprogramming.py
+++ dynamicprogramming.py
@@ -8,9 +8,40 @@ def knapSack(items, w, n):
     else:
         return max(items[n-1][1] + knapSack(items, w-items[n-1][1], n-1), knapSack(items, w, n-1))
 
-n = 4
-items = [[1,2],[3,6],[4,3],[6,7]]
-w = 12
-print(knapSack(items, w, n))
+
+with open('input.txt', "r") as f:
+    test_case_count = 1
+    lines = f.readlines()  # Read all lines into a list
+    i = 0
+
+    while i < len(lines):
+        if lines[i] == '\n':
+            total_sublists = int(lines[i + 1])
+            my_list = eval(lines[i + 2])
+            capacity = int(lines[i + 3])
+
+            with open("dp_output.txt", "a") as output_file:
+                output_file.truncate()
+                max_sum = knapSack(my_list, capacity, total_sublists)
+                #sum of indexes of the sublists
+                output_file.write("Case #" + str(test_case_count) + '\n')
+                output_file.write("Value: " + str(max_sum) + '\n')
+                output_file.write("--------------------------------------------------\n\n")
+
+            total_sublists = 0
+            my_list = []
+            capacity = 0
+
+            # Update the index to skip the processed lines
+            i += 4
+            test_case_count += 1
+        else:
+            i += 1  # Move to the next line
+
+
+
+
+
+
 
 
